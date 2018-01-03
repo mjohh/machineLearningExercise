e987a51dd5cb5c0aa705950b5337c027e4a6798d
mjohh <mjohh@163.com>
2018-01-03 19:41:48 +0800

    new assigment

---
+++ ex7/ex7/findClosestCentroids.m
@@ -0,0 +1,49 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+K
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+
+for i = 1 : size(X,1)
+	mind = sum((centroids(1,:)-X(i,:)) .* (centroids(1,:)-X(i,:))); %init the min distace
+	minc = 1;
+	%%fprintf("\ninit c for X[%d]\n", i);
+	%%mind
+	%%minc
+	for j = 2 : K %start from 2nd centroid
+		%%K
+		%%fprintf('\n    j = %d, new distance=%d', j,sum( (centroids(j,:)-X(i,:)) .* (centroids(j,:)-X(i,:))) )
+		if sum((centroids(j,:)-X(i,:)) .* (centroids(j,:)-X(i,:))) < mind
+			mind = sum((centroids(j,:)-X(i,:)) .* (centroids(j,:)-X(i,:)));
+			minc = j;
+		end
+		%%fprintf('\n    minc = %d', minc)
+	end
+	idx(i) = minc;
+end
+
+
+
+
+
+% =============================================================
+
+end
+
